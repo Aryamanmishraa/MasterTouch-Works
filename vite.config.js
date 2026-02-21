5a767f959c628ec0d3bd979e975bb29a7a453901
7884889 <97789950+7884889@users.noreply.github.com>
2026-02-21 16:18:21 +0530

    Revert "Fix GH Pages redirect base"
    This reverts commit eb5ad680c7085c4dee406eff0f8c7614adf3ca1b.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const isProd = process.env.NODE_ENV === 'production'
-  const fallbackBase = isProd ? '/MasterTouch-Works/' : '/'
-
-  return {
-    plugins: [react()],
-    base: process.env.VITE_BASE_PATH || fallbackBase,
-  }
-})
+export default defineConfig(() => ({
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/',
+}))
